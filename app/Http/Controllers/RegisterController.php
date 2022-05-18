e19f1b16dfd372479056b7d20deb6040442ec4b8
rochmadnf <rochmadnf@gmail.com>
2022-05-18 19:15:55 +0800

    auth

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+  public function signUp()
+  {
+    $validator = Validator::make(request()->all(), [
+      'username' => 'required|unique:users',
+      'email' => 'required|email|unique:users',
+      'password' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->messages());
+    }
+
+    $user = User::create(
+      [
+        'username' => request('username'),
+        'email' => request('email'),
+        'password' => bcrypt(request('password')),
+      ]
+    );
+
+    return response()->json(['message' => 'Successfully registered!']);
+  }
+}
